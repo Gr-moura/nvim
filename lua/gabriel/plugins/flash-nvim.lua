fc1d193dcba7560acbca1b0a0bb25de07895a33a
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-08-06 02:59:53 -0300

    Added flash.nvim

---
+++ lua/gabriel/plugins/flash-nvim.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+    -- stylua: ignore
+	config = function()
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ls", '<cmd>lua require("flash").jump()<cr>', { desc = "Flash jump" })
+		keymap.set("n", "<leader>lt", '<cmd>lua require("flash").treesitter()<cr>', { desc = "Flash Treesitter" })
+		keymap.set(
+			"n",
+			"<leader>lr",
+			'<cmd>lua require("flash").treesitter_search()<cr>',
+			{ desc = "Flash Treesitter Search" }
+		)
+	end,
+}
